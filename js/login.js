8ee4308c0f4ac7677da8b6f39144695207b5e09d
renzo-milla <75855277@certus.edu.pe>
2021-11-17 17:03:12 -0500

    subir

---
+++ js/login.js
@@ -0,0 +1,26 @@
+// Movernos de login a registro correctamente
+
+var x=document.getElementById("login");
+var y=document.getElementById("register");
+var z=document.getElementById("btn");
+
+function register(){
+    x.style.left="-400px";
+    y.style.left="50px";
+    z.style.left="110px";
+}
+
+function login(){
+    x.style.left="50px";
+    y.style.left="450px";
+    z.style.left="0px";
+}
+
+//Cuando damos click fuera de login o registro
+
+var modal = document.getElementById("login-form");
+window.onclick = function(event){
+    if (event.target==modal){
+        modal.style.display="none";
+    }
+}
